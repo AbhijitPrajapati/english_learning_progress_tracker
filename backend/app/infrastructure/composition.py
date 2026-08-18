cfd4c0421e4ec5834a8cddf7248b997394b10b62
AbhijitPrajapati <talk.abhijitp@gmail.com>
2026-08-18 12:28:29 -0400

    fix composition and remove uneeded backend deps

--- backend/app/infrastructure/composition.py
+++ backend/app/infrastructure/composition.py
@@ -9,7 +9,6 @@ from .grammar_analysis import OpenAIGrammarAnalysisAdapter
 from .logging import logging_setup
 from .password_hasher import PwdLibPasswordHasher
 from .token_service import JwtTokenService
-from .transcription import WhisperTranscriptionAdapter
 
 
 class InfrastructureComposition:
@@ -18,8 +17,7 @@ class InfrastructureComposition:
         self.engine = create_engine(settings.postgres)
         session_factory = create_session_factory(self.engine)
         self.uow_factory = SqlAlchemyUnitOfWorkFactory(session_factory)
-        self.transcriber = WhisperTranscriptionAdapter(settings.whisper)
-        self.grammar_analyzer = OpenAIGrammarAnalysisAdapter(settings.llm)
+        self.grammar_analyzer = OpenAIGrammarAnalysisAdapter(settings.openai)
         self.password_hasher = PwdLibPasswordHasher()
         self.token_service = JwtTokenService(settings.jwt)
 
